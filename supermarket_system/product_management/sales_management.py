46eaf60d24d8a54a1cdf9bc914015537e80847c3
zhaojr23 <zhaojingranjr23@163.com>
2024-11-29 16:34:14 -0800

    update sale managment

--- supermarket_system/product_management/sales_management.py
+++ supermarket_system/product_management/sales_management.py
@@ -1,4 +1,5 @@
-class sales():
+from supermarket_system.product_management.warehouse_management import warehouse
+class sales(warehouse):
     """
     A class to manage sales-related operations, including products on shelves.
 
@@ -24,30 +25,31 @@ class sales():
     """
     def __init__(self,product):
         self.shelves={}
-        self.product = product
+        super().__init__()
+        self.products = product.products
     def add_newproduct(self, productId, quantity, saleprice):
-        if productId not in self.shelves and productId in self.product.products and quantity <= self.product.products[productId]["quantity"]:
+        if productId not in self.shelves and productId in self.products and quantity <= self.products[productId]["quantity"]:
             self.shelves[productId]={
-                "name": self.product.products[productId]["name"],
+                "name": self.products[productId]["name"],
                 "quantity": quantity,
                 "sprice" : saleprice,
-                "pprice" : self.product.products[productId]["pprice"]
+                "pprice" : self.products[productId]["pprice"]
             }
-            self.product.remove_product(productId, quantity)
+            self.remove_product(productId, quantity)
         else:
             print("On shelves or wrong productId or quantity is too large")
 
     def increase_quantity(self, productId, quantity):
-        if productId in self.shelves and quantity <= self.product.products[productId]["quantity"]:
+        if productId in self.shelves and quantity <= self.products[productId]["quantity"]:
             self.shelves[productId]["quantity"] += quantity
-            self.product.remove_products(productId, quantity)
+            self.remove_products(productId, quantity)
         else:
             print("This product is not on the shelves")
 
     def decrease_quantity(self, productId, quantity):
         if productId in self.shelves and quantity <= self.shelves[productId]["quantity"]:
             self.shelves[productId]["quantity"] += quantity
-            self.product.products[productId]["quantity"] += quantity
+            self.products[productId]["quantity"] += quantity
         else:
             print("This product is not on the shelves")
     def change_price(self, productId, newprice):
